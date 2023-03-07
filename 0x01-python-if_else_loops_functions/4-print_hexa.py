201b88d392be3b251cab491943b489763b908ea1
Zacharia Njue <njuezachary@gmail.com>
2023-03-07 16:00:30 +0300

    added 4-print and 4-print files

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for x in range(0, 99):
+    if x <= 9:
+        x
+
+    print(x)
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for num in range(0, 100):
+    print("{}".format(num), end=", ")
