0989726d57aab00a036b8aa18fb439d80e600a9f
Zacharia Njue <njuezachary@gmail.com>
2023-03-09 20:08:16 +0300

    modified some files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     import hidden_4
-    # Print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that do not start with __
-        if name[:2] != '__':
-            print("{}".format(name))
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
