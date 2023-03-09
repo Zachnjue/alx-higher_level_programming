ac06e68cc288ed3e1251ebe1bd60d4f60caf2cb6
Zacharia Njue <njuezachary@gmail.com>
2023-03-09 15:25:46 +0300

    added 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as add1
+    import add_0
     a = 1
     b = 2
     # print(add1.add(a, b))
-    print(f"{a} + {b} = {(add1.add(a, b))}")
+    print(f"{a} + {b} = {(add_0.add(a, b))}")
