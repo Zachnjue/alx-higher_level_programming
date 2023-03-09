a36a4aa6401f91f7058ede5522d3e81771af3782
Zacharia Njue <njuezachary@gmail.com>
2023-03-09 15:24:31 +0300

    added 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import add_0 as add1
+    a = 1
+    b = 2
+    # print(add1.add(a, b))
+    print(f"{a} + {b} = {(add1.add(a, b))}")
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+ALX assignment on Modules in Python
