ca9df7bfcd67871554c6d1f3cf49376e39b252e0
Zacharia Njue <njuezachary@gmail.com>
2023-03-13 15:17:30 +0300

    modified print matrix file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    """
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
-            # print("{:d}".format(matrix[i][j]), end='')
-        print()
-    """
     for row in matrix:
-       # print(row)
         for column in row:
-            #    print(column)
             if column == row[-1]:
                 print('{:d}'.format(column), end='')
             else:
